cf8e0b8dc39b2523b52eef1203f150066d923070
MarziaJalili <astronomyfancy@gmail.com>
2024-11-10 05:41:27 +0430

    Make delivery Options interactice

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart, calculateCartQuantity, updateQuantity } from "../data/cart.js";
+import { cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
@@ -163,7 +163,10 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
         const priceString = deliveryOption.priceCents === 0
             ? "FREE"
             : `${formatCurrency(deliveryOption.priceCents)} - `;
-        html += `<div class="delivery-option">
+        html += `<div class="delivery-option js-delivery-option"
+            data-product-id="${matchingProduct.id}"        
+            data-delivery-option-id="${deliveryOption.id}"        
+        >
             <input type="radio" 
               ${deliveryOption.id === cartItem.deliveryOptionId ? "checked" : ""}
               class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
@@ -180,4 +183,13 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
         `
     });
     return html;
-}
+};
+
+document.querySelectorAll(".js-delivery-option")
+    .forEach(element => {
+        element.addEventListener("click", () => {
+            const { productId, deliveryOptionId } = element.dataset;
+            updateDeliveryOption(productId, deliveryOptionId);
+            console.log(deliveryOptionId);
+        })
+    })
