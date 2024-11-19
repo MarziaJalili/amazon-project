6fa63f7b0511017ecbabe9f93b9f388a3364935c
MarziaJalili <astronomyfancy@gmail.com>
2024-11-19 05:54:12 +0430

    Convert loadProducts from XML to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,18 +1,15 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 // import "../data/cart-class.js"
 // import "../data/car.js";
 // import "../data/backend-practice.js"
 
+
 Promise.all([
-    new Promise(resolve => {
-        loadProducts(() => {
-            resolve("value1");
-        })
-    }),
+    loadProductsFetch(),
     new Promise(resolve => {
         loadCart(() => {
             resolve();
@@ -24,6 +21,8 @@ Promise.all([
     renderOrderSummary();
     renderPaymentSummary();
 })
+
+
 
 
 /*
@@ -59,5 +58,6 @@ loadProducts(() => {
     });
 });
 */
+
 
 
